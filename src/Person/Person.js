207b1a76baef0f9e4c7bf4f130afcc1d5e73b694
alejandroem <alejandro@bixlabs.com>
2020-10-21 17:46:49 +0200

    props in components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 //In it's simple form a component is a function that return jsrx
-const person = () => {
+const person = (props) => {
     // To execute javascript code it should be in curly braces
-    return <p> I'm a person and I am {Math.floor(Math.random() * 30)} years old!</p>;
+    return <p> I'm {props.name} a person and I am {props.age} years old!</p>;
 };
 
 export default person;
