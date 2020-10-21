89490ee8994f5dcf9025feab6e0c33c4edd00202
alejandroem <alejandro@bixlabs.com>
2020-10-21 17:49:42 +0200

    props children

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,12 @@ import React from 'react';
 //In it's simple form a component is a function that return jsrx
 const person = (props) => {
     // To execute javascript code it should be in curly braces
-    return <p> I'm {props.name} a person and I am {props.age} years old!</p>;
+    return (
+    <div>
+        <p> I'm {props.name} a person and I am {props.age} years old!</p>
+        <p>{props.children}</p>
+    </div>
+    )
 };
 
 export default person;
