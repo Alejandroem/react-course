9339a75d4c0fac07b2e45610a230bae9b5c49db0
alejandroem <alejandro@bixlabs.com>
2020-10-21 18:00:58 +0200

    state in components and click handler

--- src/App.js
+++ src/App.js
@@ -4,15 +4,28 @@ import Person from './Person/Person';
 
 // Class component
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: '28' },
+      { name: 'Manu', age: '29' },
+      { name: 'Stephanie', age: '26' }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('was click');
+  }
+
   render() {
     return (
       // Wrap everything into one component
       <div className="App">
         <h1> Hi, I'm a react App </h1>
         <p> This is really working!</p>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29"> My Hobbies: Racing </Person>
-        <Person name="Stephanie" age="26"/>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].name}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].name}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].name}/>
       </div>
     );
     /*
